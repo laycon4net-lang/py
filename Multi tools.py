e0fce83c41e1fecefb5d3cd47204d1ee0ed9af1d
laycon4net-lang <laycon4net@gmail.com>
2026-05-21 19:19:19 +0100

    Add files via upload

--- Multi tools.py
+++ Multi tools.py
@@ -22,7 +22,7 @@ def looks_incomplete(text):
 def generate_complete(prompt, temprature, tokens):
      ans = generate(prompt, temprature, tokens )
      if looks_incomplete(ans):
-          cont = generate(
+          cont = generate (
                f"Continue from where you stopped. Do not reapeat.\n\n{ans},"
                temprature,
                tokens
@@ -37,4 +37,41 @@ def export_txt(history):
 def run_ai_teaching_assistant():
      st.title(" AI Teaching Assistant")
      st.session_state.setdefault("history_ata", [])
-     temp = st.
+     temp = st.slider("Creativity level", 0.0, 1.0, 0.3)
+     tokens = st.slider("Max Tokens (Response Length)", 200, 2000, 1000)
+     col1, col2 = st.colums([1,2])
+     if col1.button(" Clear"):
+          st.session_state.history_ata:
+          col2.download_button(
+               "Export",
+               export_txt(st.session_state.history_ata),
+               "Teaching_Assistant_chat.txt",
+               "text/plain"
+          )
+          q = st.text_input("Enter your question:")
+          if st.button("Ask"):
+               if not q.strip():
+                    st.warming("Enter a question.")
+                else:
+                    with st.spinner("Thinking..."):
+                        prompt = q
+                        if st.session_state.history_ata:
+                            previous = "\n".join(
+                                 [f"Q: {h['question']}\nA: {h['answer']}"
+                            )
+                            for h in st.session_state.history_ata[:3]]
+                            )
+                            prompt = previous + "\nCurrent Question: " + q
+                            ans = generate_Complete(prompt, temp, tokens)
+                           if not st.session_state.History_ata:
+                              return.markdown(" Conversation History")
+                            for i, h in enumerate(st.session_state.history_ata, 1):
+                                st.markdown(f**Q[i]: {h['question']}**")
+                                st.markdown(h["answer"])
+                                st.markdown("")
+
+                                
+
+          
+    
+         
