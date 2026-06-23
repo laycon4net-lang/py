a26e13f5aa8b6a56296b9a1a0dfa2f5c98d91fe1
laycon4net-lang <laycon4net@gmail.com>
2026-06-23 11:47:35 +0100

    Add files via upload

---
+++ mark.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+students_name=["jimmy", "Timmi", "Rayan", "bage", "louis"]
+students_marks=[50,70,67,100,82]
+marks_perc = []
+for x in students_marks:
+    res = (x/50)*100
+    marks_perc.append(res)
+    print(marks_perc)
+def marks_line_chart():
+    plt.plot(students_name,students_marks)
+    plt.title("student atendance")
+    plt.xlabel("student Atendance names")
+    plt.ylabel("score")
+    plt.show()
+marks_line_chart()
+def percentage_bar_chart():
+    plt.bar(students_name,marks_perc)
+    plt.title("student atendance")
+    plt.xlabel("student Atendance names")
+    plt.ylabel("score")
+    plt.show()
+percentage_bar_chart()
