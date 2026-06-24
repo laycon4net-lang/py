e4f8a0b90a6978cc8d8adabff4b672ff2683e34b
laycon4net-lang <laycon4net@gmail.com>
2026-06-24 11:43:19 +0100

    Add files via upload

---
+++ 25.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', "Matthew", 'Laura', 'kevin', 'Jonas'],
+'Highest marks': [45.5, 9, 35.5, np.nan, 9, 36, 22.5, np.nan, 8, 19],
+'Lowest marks': [100, 85, 24, 93, 67, 3, 18, 87, 20, 10],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'no', 'no', 'yes', 'no' ]}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+df = pd.DataFrame(exam_data , index=labels)
+print("summary of the basic informmation about this DataFrame and its data:")
+print(df.info())
