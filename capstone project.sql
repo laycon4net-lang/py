76d5fef5bc838e87834a07588649731d796a3b50
laycon4net-lang <laycon4net@gmail.com>
2026-08-03 18:56:13 +0100

    Add files via upload

--- capstone project.sql
+++ capstone project.sql
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS Customer(
     grade TEXT,
     Salesman_id TEXT
 );
-INSERT INTO CUSTERMER(customer_id,cust_name,city,grade,Salesman_id)
+INSERT INTO Customer (customer_id,cust_name,city,grade,Salesman_id)
 VALUES
 ("3002","nick rimando","new york","100","5001"),
 ("3007", "brad davis", "new york","200","5001"),
@@ -28,4 +28,30 @@ VALUES
 ("3004","fabiam johnson","paris","300","5006"),
 ("3009","geoff cameron","berlin","100","5003"),
 ("3003","jozy altidor","moscow","200","5007"),
-("3001",)
+("3001","brad guzan","london","","5005");
+CREATE TABLE IF NOT EXISTS Orders(
+    ord_no TEXT PRIMARY KEY,
+    Purch_amt TEXT,
+    ord_date TEXT,
+    customer_id TEXT,
+    Salesman_id TEXT,
+);
+INSERT INTO Orders(ord_no,purch_amt,ord_date,customer_id,Salesman_id)
+VALUES
+  ("70001","150.5","2012-10-05","3005","5002"),
+  ("70009","270.65","2012-09-10","3001","5001"),
+  ("70002","65.26","2012-10-05","3002","5003"),
+  ("70004","110.5","2012-08-17","3009","5007"),
+  ("70007","948.5","2012-09-10","3005","5005"),
+  ("70005","2400.6","2012-07-27","3007","5006");
+  --Queries
+  --Matchingcustomers and Salesman by city
+  SELECT customer.cust_name, Salesman.name, Salesman.city
+  FROM Customer
+  JOIN Salesman ON Customer.city = Salesman.city;
+  SELECT Customer.cust_name, Salesman.name
+FROM Customer
+JOIN Salesman ON Customer.Salesman_id = Salesman.Salesman_id;
+SELECT Orders.ord_no, Customer.cust_name, Orders.Salesman_id
+FROM Orders
+JOIN 
