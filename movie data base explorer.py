abcc7b777c9d7e1dff71d3427f037d45d69559c0
laycon4net-lang <laycon4net@gmail.com>
2026-08-24 18:58:09 +0100

    Add files via upload

--- movie data base explorer.py
+++ movie data base explorer.py
@@ -25,4 +25,14 @@ INSERT INTO MOVIE VALUES
 (5,'Spider-Man','Action',2002,7.3,121),
 (12,'Interstellar','Drama',2014,8.6,169);
 INSERT INTO Actor VALUES
-  (1,'Tom Hanks',1956,'USA')
+  (1,'Tom Hanks',1956,'USA'),
+  (2,'Idris Elba',1972,'UK'),
+  (3,'Chadwick Boseman',1976,'USA'),
+  (4,'Scarlettt Johnson',1984,'1984,'USA')
+  (5,'Macaulay Culkin',1980,'USA'),
+  INSERT INTO Movie_Actor VALUES
+  (1,2),(2,1),(5,1),(6,3),(6,8),(7,4),(8,7),(9,5),(11,2),(12,1);
+  """)
+conn.commit()
+print('Database ready!')
+  
